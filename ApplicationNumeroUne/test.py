59fb338c5969304ba4e17cba8b76ea85e80ff2e9
4lexiiissss <alexisdemol.europe@gmail.com>
2024-05-07 17:16:26 +0200

    07/05 - Création d'un fichier test pour le systéme de quadrillage.

--- ApplicationNumeroUne/test.py
+++ ApplicationNumeroUne/test.py
@@ -1,12 +1,13 @@
 # Code provisoire pour les créations de case pour les rayons. Système quand on clique sur une case, une fenetre modale s'ouvre
 # pour voir les produits disponible dans celui ci.
-# La case ne se met pas directement sur l'image ! Il faut modifier cela.
+# Les cases sont mis directement sur l'image, malgrès ca il y a quand meme quelque soucis car cela se positionne pas parfaitement sur une bonne partie des images.
 
 # Alexis Demol TPB
 
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QDialog
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 
 class Plateau(QWidget):
     def __init__(self):
@@ -15,22 +16,33 @@ class Plateau(QWidget):
         layout = QVBoxLayout(self)
 
         # Plateau
-        pixmap = QPixmap(sys.path[0] + '/icones/plan1.jpg')
-        image_label = QLabel(self)
-        image_label.setPixmap(pixmap)
-        layout.addWidget(image_label)
+        pixmap = QPixmap(sys.path[0] + '/icones/plan11.jpg')
+        self.image_label = QLabel(self)
+        self.image_label.setPixmap(pixmap)
 
-        # case
-        case = QLabel(self)
-        case.setFixedSize(100, 100)  
-        case.setStyleSheet("border: 1px solid black;") 
-        case.mousePressEvent = self.ouvrir_fenetre
-        layout.addWidget(case)
+        layout.addWidget(self.image_label)
+        self.createQuadrillage()
 
         self.setLayout(layout)
 
+    def createQuadrillage(self):
+        # Dimensions de la grille
+        lgn = 30
+        cols = 30
+        cellLarg = self.image_label.pixmap().width() // cols
+        cellHaut = self.image_label.pixmap().height() // lgn
+
+        for row in range(lgn):
+
+            for col in range(cols):
+
+                cell = QLabel(self.image_label)
+                cell.setGeometry(col * cellLarg, row * cellHaut, cellLarg, cellHaut)
+                cell.setStyleSheet("border: 1px solid black;") # bordure en noir pour voir les zones ou on peut cliquer 
+                cell.mousePressEvent = self.ouvrirFenetre
+
     # ouverture fenetre 
-    def ouvrir_fenetre(self, event):
+    def ouvrirFenetre(self, event):
         fenetre = FenetreTexte("\n Légumes")
         fenetre.exec()
 
@@ -38,7 +50,7 @@ class FenetreTexte(QDialog):
     def __init__(self, text):
         super().__init__()
 
-        self.setWindowTitle("Fenetre texte test.")
+        self.setWindowTitle("Fenetre texte test")
         label = QLabel(f"Contenu du rayon: {text}")
 
         layout = QVBoxLayout()
